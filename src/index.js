15bdb07316229bd93d766f99781ca84b5823b74f
Michael Friedman <minorpianokeys@gmail.com>
2023-08-21 17:41:09 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    buildToDo(e.target["new-task-description"].value);
+    form.reset();
+  })
 });
+
+function buildToDo(todo) {
+
+  if (todo === '') {
+    alert('You must submit Todo!')
+  } else {
+    let li = document.createElement('li');
+  li.innerHTML = `${todo} `
+  let exitBtn = document.createElement('button');
+  exitBtn.innerHTML = 'x'
+  exitBtn.addEventListener('click', handleDelete)
+
+  li.appendChild(exitBtn);
+  document.querySelector('#tasks').appendChild(li);
+  }
+
+  
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
